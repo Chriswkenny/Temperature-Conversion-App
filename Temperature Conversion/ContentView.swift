13f315421ad0ab082cbdb6006642808d3e8a2999
Chris Kenny <s1139346@monmouth.edu>
2026-09-17 14:47:30 -0400

    Initial Commit

---
+++ Temperature Conversion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Temperature Conversion
+//
+//  Created by Christopher W. Kenny on 9/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Temperature Conversion/Temperature_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConversionApp.swift
+//  Temperature Conversion
+//
+//  Created by Christopher W. Kenny on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
